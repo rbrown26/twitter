1fe1ecee2079d613abb1e36758a092e7bc785113
Richard Brown <rbrown26@citadel.edu>
2019-11-24 16:32:24 -0500

    registration and login complete

--- twitter/src/components/Login.js
+++ twitter/src/components/Login.js
@@ -2,47 +2,84 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import Header from './Header';
 import MainNav from './MainNav';
+import {userService} from "../services/user.service";
 
 class Login extends Component {
-  state = {
-    username: "",
-    password: ""
-  };
 
-  handleValueChange = (e) => {
-    this.setState({ value: e.target.value });
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        username: "",
+        password: ""
+      };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
 
-  render() {
-    return (
-      <div>
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('in handleSubmit');
+    this.setState({ submitted: true });
+    const { username, password, returnUrl } = this.state;
+    console.log('updated state');
+    console.log('username is ' + username);
+    console.log('password is ' + password);
+    if (!(username && password)) {
+      return;
+    }
+    console.log('got what we need');
+    this.setState({ loading: true });
+    console.log('calling user service');
+    userService.login(username, password)
+        .then(
+            user => {
+              const { from } = this.props.location.state || { from: { pathname: "/profile" } };
+              this.props.history.push(from);
+            },
+            error => {
+              this.setState({ error, loading: false })
+            }
+        );
+
+}
+
+
+render() {
+  const { username, password, submitted, loading, error } = this.state;
+  return (
+      <div class="App">
         <Header />
-        <MainNav />
+        <MainNav
+          navClass="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
 
-        <form>
-          <input
-            className="Login"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleValueChange}
-            placeholder="Username"
-          />
-
-          <input
-            className="Login"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleValueChange}
-            placeholder="Password"
-          />
-
-          <input
-            className="LoginSubmit"
-            type="submit"
-            value="Login"
-          />
-        </form>
+        />
+        <div className="container main">
+          <h1 class="Register">Sign in</h1>
+          <form name="loginForm" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <div className="row">
+                <div className="col-md-5">
+                  <label htmlFor="username">Username</label>
+                  <input className="form-control" id="username" onChange={this.handleChange} placeholder="Username"/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-5">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" className="form-control" id="password" onChange={this.handleChange} placeholder="Password"/>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+            { error && <div className={'alert alert-danger'}>{error}</div> }
+          </form>
+        </div>
       </div>
     );
   }
--- twitter/src/components/MainNav.js
+++ twitter/src/components/MainNav.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import { userService } from "../services/user.service";
 
 class MainNav extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+          loggedIn: true
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.updateLoginState = this.updateLoginState.bind(this);
+  }
+
+  updateLoginState() {
+    this.setState({ loggedIn: true });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log('in handleClick');
+    userService.logout();
+    this.setState({ loggedIn: false });
+    document.location.href = "/logout";
+  }
+
   render() {
+    const { loggedIn } = this.state;
     const user = localStorage.getItem('user');
     let welcomeLogin;
     let logoutRegister;
 
     if (user != null) {
-      welcomeLogin = <span className="navbar-brand">Welcome back, { JSON.parse(user).firstName }!</span>
-      logoutRegister = <a className="nav-link" href="/logout">Logout</a>;;
+
+      welcomeLogin = <span className="navbar-brand">Welcome back, { JSON.parse(user).username }!</span>;
+      logoutRegister = <a className="nav-link" href="/logout" onClick={ this.handleClick } >Logout</a>;
     } else {
       welcomeLogin = <a className="nav-link" href="/login">Login</a>;
       logoutRegister = <a className="nav-link" href="/register">Register</a>;
